55f728c82f68db8cc277d818033947a95d10bbbd
Marquis Whitley <whitley.marquis1@yahoo.com>
2020-09-09 20:34:23 -0400

    js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const hamburgerContainer = document.querySelector('#header__main-nav');
 const hamburger = document.querySelector('.header__main-nav--hamburger');
 const links = document.querySelectorAll('.header__main-nav--links li');
+const navBar = document.querySelector('section');
 
 hamburger.addEventListener('click', () => {
     hamburgerContainer.classList.toggle ('clicked');
@@ -8,3 +9,20 @@ hamburger.addEventListener('click', () => {
         link.classList.toggle('fade');
     })
 });
+
+// window.addEventListener('scroll', ()=>{
+//     navBar.classList.toggle('sticky', window.scrollY > 100)
+// })
+
+window.ons
+window.addEventListener('scroll', ()=>{
+    navBar.classList.toggle('sticky', window.scrollY > 0)
+})
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = ()=>{
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + '%'
+}
